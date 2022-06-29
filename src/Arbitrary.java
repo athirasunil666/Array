dacd9a5e2ddb7b5c160467ebf9a60c7af5234bbd
Athira Sunil <athirasunil457@gmail.com>
2022-06-29 12:26:49 +0300

    Initial commit

---
+++ src/Arbitrary.java
@@ -0,0 +1,14 @@
+public class Arbitrary {
+    public static void main(String[] args) {
+        int[] myarray;
+        myarray = new int[4];
+        myarray[0] = 10;
+        System.out.println("myarray[0] = " + myarray[0]);
+        System.out.println("myarray[1] = " + myarray[1]);
+        int[] wholeArray = {1, 3, 5, 7};
+        System.out.println("wholeArray[0] = " + wholeArray[0]);
+        System.out.println("wholeArray[1] = " + wholeArray[1]);
+        System.out.println("wholeArray[2] = " + wholeArray[2]);
+        System.out.println("wholeArray[3] = " + wholeArray[3]);
+    }
+    }
